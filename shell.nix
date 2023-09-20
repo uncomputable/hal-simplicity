c0ee38346bfd008bf243670c4263c655961ad704
Christian Lewe <ch_lewe@agdsn.me>
2023-09-20 21:58:21 +0200

    Nix: Fix and update shell
    Update to latest simplicity.
    Fix bug where the same sha hash is used across versions.

--- shell.nix
+++ shell.nix
@@ -1,12 +1,12 @@
 with (import <nixpkgs> {});
 let
   elementsd-simplicity = elementsd.overrideAttrs (_: rec {
-    version = "unstable-2023-06-15";
+    version = "unstable-2023-08-24a";
     src = fetchFromGitHub {
       owner = "ElementsProject";
       repo = "elements";
-      rev = "80c5d581ff2a5c9d0d2f759f84575a9fe8204efa"; # <-- update this to latest `simplicity` branch: https://github.com/ElementsProject/elements/commits/simplicity
-      sha256 = "ooe+If3HWaJWpr2ux7DpiCTqB9Hv+aXjquEjplDjvhM="; # <-- ignore this
+      rev = "16fd80c3aca58e059268a11a3cba5f3c0d2607a2"; # <-- update this to latest `simplicity` branch: https://github.com/ElementsProject/elements/commits/simplicity
+      sha256 = "sha256-Sv3kMnlnXXmCYshSPwTkgEc/M6aHAQInnqCB7kXW4WY="; # <-- overwrite this, rerun and place the expected hash
     };
   });
 in
